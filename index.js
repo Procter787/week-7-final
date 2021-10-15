e3be1a2383d39eeb91c909783cf557e61fec4881
Porter Morrison <port.morrison@gmail.com>
2021-10-15 11:17:10 -0600

    changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const str = 'We promptly judged antique ivory buckles for the next prize';
+const str = 'The quick brown fox jumps over the lazy dog';
 const isPangram = (str = '') => {
    str = str.toLowerCase();
    const { length } = str;
@@ -18,10 +18,10 @@ console.log(isPangram(str));
 
 
 
-const arr = '1, -1'
+// const arr = '1, -1'
 
-const addToZero = (arr = '') => {
-  for(let i = 0; i < length; i++){
-    if()
-  }
-}
+// const addToZero = (arr = '') => {
+//   for(let i = 0; i < length; i++){
+//     if()
+//   }
+// }
